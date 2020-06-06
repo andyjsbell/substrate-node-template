d0bee22bf06836dc40084404df7f52e0dc67fc1e
Andy Bell <andy.bell@displaynote.com>
2020-06-06 20:05:12 +0200

    structure to store deposit

--- pallets/remittance/src/lib.rs
+++ pallets/remittance/src/lib.rs
@@ -1,5 +1,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+/// A FRAME pallet template with necessary imports
+
+/// Feel free to remove or edit this file as needed.
+/// If you change the name of this file, make sure to update its references in runtime/src/lib.rs
+/// If you remove this file, you can remove those references
+
+/// For more guidance on Substrate FRAME, see the example pallet
+/// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
+
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
 use frame_system::{self as system, ensure_signed};
 
@@ -17,16 +26,23 @@ pub trait Trait: system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
+struct Deposit<AccountId> {
+	remitter : AccountId,
+	expires : u32,
+	value : u32,
+}
+
 // This pallet's storage items.
 decl_storage! {
 	// It is important to update your storage name so that your pallet's
 	// storage items are isolated from other pallets.
 	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as RemittanceModule {
+	trait Store for Module<T: Trait> as TemplateModule {
 		// Just a dummy storage item.
 		// Here we are declaring a StorageValue, `Something` as a Option<u32>
 		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
 		Something get(fn something): Option<u32>;
+		
 	}
 }
 
